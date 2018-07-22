f282cf879cb41deb17765a0c192ac447a23c916c
Rajesh Prabhu <rajesh.prabhu@sap.com>
2018-07-22 16:51:44 +0530

    additional impl

---
+++ calculator.py
@@ -0,0 +1,16 @@
+a = int(input("Enter first number"))
+b = int(input("Enter second number"))
+c = enter(input("Enter the calculation operation"))
+
+if c == "+":
+	print(a+b)
+if c == "-":
+	print(a-b)
+if c == "*":
+	print(a*b)
+if c == "/":
+	if b = 0:
+		print("Div by zero illegal")
+	else:
+		print(a/b)
+
