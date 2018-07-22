f3e87c10796217fccedfdae97afe3bafd4ff6b84
Rajesh Prabhu <rajesh.prabhu@sap.com>
2018-07-22 16:57:23 +0530

    second commit

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 a = int(input("Enter first number"))
 b = int(input("Enter second number"))
-c = enter(input("Enter the calculation operation"))
+c = str(input("Enter the calculation operation"))
 
 if c == "+":
 	print(a+b)
@@ -9,7 +9,7 @@ if c == "-":
 if c == "*":
 	print(a*b)
 if c == "/":
-	if b = 0:
+	if b == 0:
 		print("Div by zero illegal")
 	else:
 		print(a/b)
