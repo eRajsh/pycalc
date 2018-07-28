114d4f06f0d36c0fba821e2c74ac43e78be9bd8e
eRajsh <rajesh.prabhu@sap.com>
2018-07-28 14:41:50 +0530

    testing

--- PyCalcClass.py
+++ PyCalcClass.py
@@ -4,22 +4,22 @@ class Calc():
 
     def add(self, a, b):
         result = a + b
-        self.prt(self, ("Result = ", result))
+        self.prt("Result = " + str(result))
 
     def sub(self, a, b):
         result = a - b
-        self.prt(self, ("Result = ", result))
+        self.prt("Result = " + str(result))
 
     def div(self, a, b):
-        if (a == 0):
-            self.prt(self, "illegal operation, number cannot be zero")
+        if (b == 0):
+            self.prt("illegal operation number cannot be zero")
         else:
             result = a / b
-            self.prt(self, ("Result = ", result))
+            self.prt("Result = " + str(result))
 
     def mult(self, a, b):
         result = a * b
-        self.prt(self, ("Result = ", result))
+        self.prt("Result = " + str(result))
 
     def prt(self, msg):
         print(msg)
@@ -35,7 +35,7 @@ if c == "+":
     calc.add(a, b)
 
 if c == "-":
-    calc.sub(calc)
+    calc.sub(a, b)
 
 if c == "*":
     calc.mult(a, b)
